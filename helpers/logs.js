52e3ee00ba5941795a54026d38abefc752b968ff
iSky-bit <54770799+iSky-bit@users.noreply.github.com>
2021-05-04 15:08:47 +0300

    Divided the utils into utils and logs messages

---
+++ helpers/logs.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.infoLogEnd = exports.infoShopLog = exports.infoHuntLog = exports.infoLog = void 0;
+var infoLog = function () {
+    console.log('------------Info------------');
+};
+exports.infoLog = infoLog;
+var infoHuntLog = function () {
+    console.log('------------Hunt------------');
+};
+exports.infoHuntLog = infoHuntLog;
+var infoShopLog = function () {
+    console.log('------------Shop------------');
+};
+exports.infoShopLog = infoShopLog;
+var infoLogEnd = function () {
+    console.log('----------------------------\n');
+};
+exports.infoLogEnd = infoLogEnd;
---
+++ helpers/logs.ts
@@ -0,0 +1,15 @@
+export const infoLog = () => {
+  console.log('------------Info------------');
+};
+
+export const infoHuntLog = () => {
+  console.log('------------Hunt------------');
+};
+
+export const infoShopLog = () => {
+  console.log('------------Shop------------');
+};
+
+export const infoLogEnd = () => {
+  console.log('----------------------------\n');
+};
