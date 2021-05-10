82d8fa2d8d02e93a8a4e51af8b589a40b4145067
iSky-bit <54770799+iSky-bit@users.noreply.github.com>
2021-05-10 15:18:24 +0300

    Update logs.js

--- helpers/logs.js
+++ helpers/logs.js
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.infoLogEnd = exports.infoShopLog = exports.infoHuntLog = exports.infoLog = void 0;
-var infoLog = function () {
+const infoLog = () => {
     console.log('------------Info------------');
 };
 exports.infoLog = infoLog;
-var infoHuntLog = function () {
+const infoHuntLog = () => {
     console.log('------------Hunt------------');
 };
 exports.infoHuntLog = infoHuntLog;
-var infoShopLog = function () {
+const infoShopLog = () => {
     console.log('------------Shop------------');
 };
 exports.infoShopLog = infoShopLog;
-var infoLogEnd = function () {
+const infoLogEnd = () => {
     console.log('----------------------------\n');
 };
 exports.infoLogEnd = infoLogEnd;
