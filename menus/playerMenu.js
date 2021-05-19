485463decd5257db88848676fe6ad342bd125f9d
iSky-bit <54770799+iSky-bit@users.noreply.github.com>
2021-05-19 11:06:09 +0300

    Update playerMenu.js

--- menus/playerMenu.js
+++ menus/playerMenu.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.playerMenu = void 0;
+const utils_1 = require("../helpers/utils");
 const playerUtils_1 = require("../player/playerUtils");
+const playMenu_1 = require("./playMenu");
 const inquirer = require('inquirer');
 const playerMenu = (player) => {
     return inquirer
@@ -10,19 +12,33 @@ const playerMenu = (player) => {
             type: 'list',
             name: 'choice',
             message: 'Please select a option!',
-            choices: ['Profile', 'Inventory', 'Stats', 'Back'],
+            choices: ['Profile', 'Inventory', 'Stats', 'Perks', 'Back'],
         },
     ])
         .then((answers) => {
         switch (answers.choice) {
             case 'Profile':
+                utils_1.cls();
                 playerUtils_1.profile(player);
+                exports.playerMenu(player);
                 break;
             case 'Inventory':
+                utils_1.cls();
                 playerUtils_1.inventory(player);
+                exports.playerMenu(player);
                 break;
             case 'Stats':
+                utils_1.cls();
                 playerUtils_1.stats(player);
+                exports.playerMenu(player);
+                break;
+            case 'Perks':
+                utils_1.cls();
+                playerUtils_1.perks(player);
+                break;
+            case 'Back':
+                utils_1.cls();
+                playMenu_1.playMenu(player);
                 break;
         }
     });
