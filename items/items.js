303d813d3d7d21973d1ff0d50b6d57ce6c71b9be
iSky-bit <54770799+iSky-bit@users.noreply.github.com>
2021-05-19 11:06:03 +0300

    Update items.js

--- items/items.js
+++ items/items.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Lighter = exports.testItem = void 0;
+exports.SteelAxe = exports.IronAxe = exports.CooperAxe = exports.Lighter = exports.testItem = void 0;
 exports.testItem = {
     type: 'Item',
     name: 'testItem',
@@ -12,4 +12,25 @@ exports.Lighter = {
     name: 'Lighter',
     desc: 'Simple lighter, produces small amount of light.',
     value: 10,
+};
+exports.CooperAxe = {
+    type: 'Axe',
+    name: 'Cooper Axe',
+    desc: 'Best axe for begginers.',
+    value: 80,
+    axePower: 5,
+};
+exports.IronAxe = {
+    type: 'Axe',
+    name: 'Iron Axe',
+    desc: 'Mid-tier axe.',
+    value: 200,
+    axePower: 10,
+};
+exports.SteelAxe = {
+    type: 'Axe',
+    name: 'Steel Axe',
+    desc: 'The perfect axe for experts.',
+    value: 400,
+    axePower: 20,
 };
