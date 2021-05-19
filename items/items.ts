fea1c397cf949f273c610e3e882c2b45316b9ba8
iSky-bit <54770799+iSky-bit@users.noreply.github.com>
2021-05-19 10:45:35 +0300

    Update items.ts

--- items/items.ts
+++ items/items.ts
@@ -19,3 +19,27 @@ export const Lighter: Item = {
   desc: 'Simple lighter, produces small amount of light.',
   value: 10,
 };
+
+export const CooperAxe: Item = {
+  type: 'Axe',
+  name: 'Cooper Axe',
+  desc: 'Best axe for begginers.',
+  value: 80,
+  axePower: 5,
+};
+
+export const IronAxe: Item = {
+  type: 'Axe',
+  name: 'Iron Axe',
+  desc: 'Mid-tier axe.',
+  value: 200,
+  axePower: 10,
+};
+
+export const SteelAxe: Item = {
+  type: 'Axe',
+  name: 'Steel Axe',
+  desc: 'The perfect axe for experts.',
+  value: 400,
+  axePower: 20,
+};
