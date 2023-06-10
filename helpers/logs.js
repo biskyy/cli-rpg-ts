8bc0e8241e748258020f36996e79d73c4bf5bc1e
biskyy <54770799+biskyy@users.noreply.github.com>
2023-06-10 20:44:30 +0300

    New logging functions
    Added skip typing feature to the typewriter(will sometimes enter a new line(bug)), also added printBattleInfo function which takes the player and the monster the player is fighting and print their information to the console.

--- helpers/logs.js
+++ helpers/logs.js
@@ -1,6 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.typewriter = exports.infoLogEnd = exports.infoShopLog = exports.infoHuntLog = exports.infoLog = void 0;
+exports.playerConfirmation = exports.printBattleInfo = exports.typewriter = exports.infoLogEnd = exports.infoVSLog = exports.infoShopLog = exports.infoHuntLog = exports.infoLog = void 0;
+const inquirer_1 = __importDefault(require("inquirer"));
+const player_1 = require("../player/player");
+const readline_1 = __importDefault(require("readline"));
+readline_1.default.emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY)
+    process.stdin.setRawMode(true);
 const infoLog = () => {
     console.log('------------Info------------');
 };
@@ -13,14 +22,57 @@ const infoShopLog = () => {
     console.log('------------Shop------------');
 };
 exports.infoShopLog = infoShopLog;
+const infoVSLog = () => {
+    console.log('-------------VS-------------');
+};
+exports.infoVSLog = infoVSLog;
 const infoLogEnd = () => {
     console.log('----------------------------');
 };
 exports.infoLogEnd = infoLogEnd;
+// export const typewriter = (text: string, delay?: number) => {
+//   return new Promise((resolve) => {
+//     let index = 0;
+//     const type = () => {
+//       if (index < text.length) {
+//         process.stdout.write(text.charAt(index));
+//         index++;
+//         setTimeout(type, delay ? delay : 30);
+//       } else {
+//         process.stdout.write('\n');
+//         resolve(0);
+//       }
+//     };
+//     type();
+//   });
+// };
 const typewriter = (text, delay) => {
+    let skipped = false; // Track if the typewriter effect was skipped
+    const handleKeyPress = (key) => {
+        if (key === '\r') {
+            skipped = true;
+            cleanup();
+        }
+    };
+    const cleanup = () => {
+        process.stdin.pause(); // Pause the stdin stream
+        process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+        process.stdin.setRawMode(false); // Disable raw mode
+        process.stdin.setEncoding('utf8'); // Set encoding back to default
+    };
+    process.stdin.setRawMode(true); // Enable reading individual keypresses
+    process.stdin.setEncoding('utf8');
+    process.stdin.resume();
+    process.stdin.on('data', handleKeyPress); // Listen for keypress events
     return new Promise((resolve) => {
         let index = 0;
         const type = () => {
+            if (skipped) {
+                process.stdout.write(text.substring(index) + '\n');
+                cleanup();
+                resolve(0);
+                return;
+            }
             if (index < text.length) {
                 process.stdout.write(text.charAt(index));
                 index++;
@@ -28,6 +80,7 @@ const typewriter = (text, delay) => {
             }
             else {
                 process.stdout.write('\n');
+                cleanup();
                 resolve(0);
             }
         };
@@ -35,3 +88,81 @@ const typewriter = (text, delay) => {
     });
 };
 exports.typewriter = typewriter;
+// export const typewriter = (text: string, delay?: number) => {
+//   let skipped = false; // Track if the typewriter effect was skipped
+//   const handleKeyPress = (key) => {
+//     if (key === '\r') {
+//       skipped = true;
+//       process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+//     }
+//   };
+//   // const cleanup = () => {
+//     // process.stdin.pause(); // Pause the stdin stream
+//     // process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+//     // process.stdin.setRawMode(false); // Disable raw mode
+//     // process.stdin.setEncoding('utf8'); // Set encoding back to default
+//   // };
+//   process.stdin.setRawMode(true); // Enable reading individual keypresses
+//   process.stdin.resume();
+//   process.stdin.setEncoding('utf8');
+//   process.stdin.on('data', handleKeyPress); // Listen for keypress events
+//   return new Promise((resolve) => {
+//     let index = 0;
+//     const type = () => {
+//       if (skipped) {
+//         // If the skipTyping flag is true, print the entire text immediately
+//         process.stdout.write(text.substring(index) + '\n');
+//         // cleanup();
+//         return;
+//       }
+//       if (index < text.length) {
+//         process.stdout.write(text.charAt(index));
+//         index++;
+//         setTimeout(type, delay ? delay : 30);
+//       } else {
+//         process.stdout.write('\n');
+//         // cleanup();
+//         resolve(0)
+//       }
+//     };
+//     type();
+//   });
+// };
+const printBattleInfo = (second, first) => {
+    let player = second instanceof player_1.Player ? second : first;
+    let enemy = first instanceof player_1.Player ? second : first;
+    (0, exports.infoLogEnd)();
+    console.log(`[Lvl ${enemy.lvl}] ${enemy.name}`);
+    console.log(`HP: ${enemy.hp}/${enemy.maxHp}`);
+    console.log(`Attack Power: ${enemy.attackPower}`);
+    console.log(`Defense Power: ${enemy.defensePower}`);
+    if (enemy.effects.length != 0) {
+        console.log('Effects:');
+        for (const effect of enemy.effects) {
+            console.log(`- ${effect.name}: ${effect.roundsRemaining}`);
+        }
+    }
+    (0, exports.infoVSLog)();
+    console.log(`[Lvl ${player.lvl}] ${player.name}`);
+    console.log(`HP: ${player.hp}/${player.maxHp}`);
+    console.log(`Attack Power: ${player.attackPower}`);
+    console.log(`Defense Power: ${player.defensePower}`);
+    if (player.effects.length != 0) {
+        console.log('Effects:');
+        for (const effect of player.effects) {
+            console.log(`- ${effect.name}: ${effect.roundsRemaining}`);
+        }
+    }
+    (0, exports.infoLogEnd)();
+    console.log('');
+};
+exports.printBattleInfo = printBattleInfo;
+const playerConfirmation = async () => await inquirer_1.default.prompt([
+    {
+        type: 'confirm',
+        message: 'Continue',
+        name: 'confirmChoice',
+        default: true,
+    },
+]);
+exports.playerConfirmation = playerConfirmation;
--- helpers/logs.ts
+++ helpers/logs.ts
@@ -1,3 +1,14 @@
+import inquirer from 'inquirer';
+import { Monster } from '../npcs/monsters';
+import { Character } from '../player/character';
+import { Player } from '../player/player';
+import { cls } from './utils';
+
+import readline from 'readline';
+
+readline.emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY) process.stdin.setRawMode(true);
+
 export const infoLog = () => {
   console.log('------------Info------------');
 };
@@ -10,24 +21,163 @@ export const infoShopLog = () => {
   console.log('------------Shop------------');
 };
 
+export const infoVSLog = () => {
+  console.log('-------------VS-------------');
+};
+
 export const infoLogEnd = () => {
   console.log('----------------------------');
 };
+
+// export const typewriter = (text: string, delay?: number) => {
+//   return new Promise((resolve) => {
+//     let index = 0;
+
+//     const type = () => {
+//       if (index < text.length) {
+//         process.stdout.write(text.charAt(index));
+//         index++;
+//         setTimeout(type, delay ? delay : 30);
+//       } else {
+//         process.stdout.write('\n');
+//         resolve(0);
+//       }
+//     };
+
+//     type();
+//   });
+// };
+
 export const typewriter = (text: string, delay?: number) => {
+  let skipped = false; // Track if the typewriter effect was skipped
+
+  const handleKeyPress = (key) => {
+    if (key === '\r') {
+      skipped = true;
+      cleanup();
+    }
+  };
+
+  const cleanup = () => {
+    process.stdin.pause(); // Pause the stdin stream
+    process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+    process.stdin.setRawMode(false); // Disable raw mode
+    process.stdin.setEncoding('utf8'); // Set encoding back to default
+  };
+
+  process.stdin.setRawMode(true); // Enable reading individual keypresses
+  process.stdin.setEncoding('utf8');
+  process.stdin.resume();
+  process.stdin.on('data', handleKeyPress); // Listen for keypress events
   return new Promise((resolve) => {
     let index = 0;
 
     const type = () => {
+      if (skipped) {
+        process.stdout.write(text.substring(index) + '\n');
+        cleanup();
+        resolve(0);
+        return;
+      }
+
       if (index < text.length) {
         process.stdout.write(text.charAt(index));
         index++;
         setTimeout(type, delay ? delay : 30);
       } else {
         process.stdout.write('\n');
+        cleanup();
         resolve(0);
       }
     };
 
     type();
+    
   });
 };
+
+// export const typewriter = (text: string, delay?: number) => {
+//   let skipped = false; // Track if the typewriter effect was skipped
+
+//   const handleKeyPress = (key) => {
+//     if (key === '\r') {
+//       skipped = true;
+//       process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+//     }
+//   };
+
+//   // const cleanup = () => {
+//     // process.stdin.pause(); // Pause the stdin stream
+//     // process.stdin.removeListener('data', handleKeyPress); // Remove the event listener
+//     // process.stdin.setRawMode(false); // Disable raw mode
+//     // process.stdin.setEncoding('utf8'); // Set encoding back to default
+//   // };
+
+//   process.stdin.setRawMode(true); // Enable reading individual keypresses
+//   process.stdin.resume();
+//   process.stdin.setEncoding('utf8');
+//   process.stdin.on('data', handleKeyPress); // Listen for keypress events
+//   return new Promise((resolve) => {
+//     let index = 0;
+
+//     const type = () => {
+//       if (skipped) {
+//         // If the skipTyping flag is true, print the entire text immediately
+//         process.stdout.write(text.substring(index) + '\n');
+//         // cleanup();
+//         return;
+//       }
+
+//       if (index < text.length) {
+//         process.stdout.write(text.charAt(index));
+//         index++;
+//         setTimeout(type, delay ? delay : 30);
+//       } else {
+//         process.stdout.write('\n');
+//         // cleanup();
+//         resolve(0)
+//       }
+//     };
+
+//     type();
+//   });
+// };
+
+export const printBattleInfo = (second: Character, first: Character) => {
+  let player = second instanceof Player ? second : first;
+  let enemy = first instanceof Player ? second : first;
+  infoLogEnd();
+  console.log(`[Lvl ${enemy.lvl}] ${enemy.name}`);
+  console.log(`HP: ${enemy.hp}/${enemy.maxHp}`);
+  console.log(`Attack Power: ${enemy.attackPower}`);
+  console.log(`Defense Power: ${enemy.defensePower}`);
+  if (enemy.effects.length != 0) {
+    console.log('Effects:');
+    for (const effect of enemy.effects) {
+      console.log(`- ${effect.name}: ${effect.roundsRemaining}`);
+    }
+  }
+  infoVSLog();
+  console.log(`[Lvl ${player.lvl}] ${player.name}`);
+  console.log(`HP: ${player.hp}/${player.maxHp}`);
+  console.log(`Attack Power: ${player.attackPower}`);
+  console.log(`Defense Power: ${player.defensePower}`);
+  if (player.effects.length != 0) {
+    console.log('Effects:');
+    for (const effect of player.effects) {
+      console.log(`- ${effect.name}: ${effect.roundsRemaining}`);
+    }
+  }
+  infoLogEnd();
+  console.log('');
+};
+
+export const playerConfirmation = async () =>
+  await inquirer.prompt([
+    {
+      type: 'confirm',
+      message: 'Continue',
+      name: 'confirmChoice',
+      default: true,
+    },
+  ]);
